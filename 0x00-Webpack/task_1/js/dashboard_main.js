daed137bef9313d682d897772b9a3929a14c4f2e
israamaher <48337918+israamaher@users.noreply.github.com>
2024-08-19 23:35:27 +0300

    task_1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,19 @@
+var $ = require("jquery");
+
+$('body').append('<p> Holberton Dashboard</p>');
+$('body').append('<p> Dashboard data for the students</p>');
+$('bode').append('<button>Click here to get started </button>');
+$('body').append("<p id='count'> </p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const updateCounter = () => {
+    let times = $('#count').html() || 0;
+	$('button').on('click', () => {
+		times++;
+		$('#count').html(`${times} clicks on the button`);
+	});
+};
+
+
+_.debounce(updateCounter, 500);
+updateCounter();
