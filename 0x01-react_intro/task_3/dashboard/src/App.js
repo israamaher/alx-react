380c19ba659997fcdaf915dd7750697c1cc48451
israamaher <48337918+israamaher@users.noreply.github.com>
2024-08-23 13:00:40 +0300

    update

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -10,13 +10,13 @@ function App() {
       </header>
       <body className="App-body">
         <p>Login to access the full dashboard </p> 
-        <section>
+        <section className="App-form">
           <fieldset>
             <label htmlfor="email"> Email:<input type="email" name="email"/> </label>
             <label htmlfor="password">password:  <input name="password" type="password"/></label>
           </fieldset>
+          <button> OK</button>
         </section>
-
       </body>
       <footer className="App-footer">
                 <p>Copyright 2020 - holberton School </p>
